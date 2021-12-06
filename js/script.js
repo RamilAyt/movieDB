002c920b82f7358181e3e80ed32ded19984a02cc
Ramil <Aytmukhametov27@gmail.com>
2021-12-06 10:35:57 +0500

    second lesson

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 let personalMovieDB = {
   count: numberOfFilms,
@@ -8,14 +8,35 @@ let personalMovieDB = {
   privat: false
 };
 
-let a = prompt('Один из последних просмотренных фильмов?', ''),
-    b = prompt('На сколько оцените его?', ''),
-    c = prompt('Один из последних просмотренных фильмов?', ''),
-    d = prompt('На сколько оцените его?', '');
 
+for (let i = 0; i < 2; i++) {
+  let a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+  if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log('done');
+  } else {
+    console.log('error');
+    i--;
+  }
+}
+
+if (personalMovieDB.count < 10) {
+  alert("Просмотрено довольно мало фильмов");
+  console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+  alert("Вы классический зритель");
+  console.log("Вы классический зритель");
+} else if (personalMovieDB.count > 30) {
+  alert("Вы киноман");
+  console.log("Вы киноман");
+} else {
+  alert("Произошла ошибка");
+  console.log("Произошла ошибка");
+}
 
 console.log(personalMovieDB); 
+
+
 
